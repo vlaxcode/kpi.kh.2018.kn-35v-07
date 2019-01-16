a321e732fd022bd3c1700af2b4bcf8527f64078b
Anna Dmitrieva <icoffee128@gmail.com>
2019-01-16 17:18:00 +0200

    Lab4Web

---
+++ ClassLibrary1/Context.cs
@@ -0,0 +1,20 @@
+﻿using Lab4_Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4_Data
+{
+    public class Context : DbContext
+    {
+        public Context()
+            : base("ResponseConnection")
+        {
+        }
+
+        public DbSet<Response> Responses { get; set; }
+    }
+}
